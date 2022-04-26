851be6415bece60d8b2485504d05726586bbff60
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-04-26 22:42:09 +0100

    Header/Nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './header.css'
+import { BsArrowUpCircleFill, BsArrowDownCircleFill } from 'react-icons/bs'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header container'>
+      <div className="header__content">
+        <h1>Buy and Sell Properties Quick and Securely.</h1>
+        <p>Helping you find the perfect property or sell your properties quickly and securely.</p>
+        <div className="header__cta">
+          <div className="cta__button">
+            <div className="buy">
+              <BsArrowUpCircleFill className='icon'/>
+              <a href="#buy">Buy</a>
+            </div>
+            <div className="sell">
+              <BsArrowDownCircleFill className='icon'/>
+              <a href="#sell">Sell</a>
+            </div>
+          </div>
+          <div className="cta__search">
+            <input type="text" placeholder='Location' />
+            <input type="text" placeholder='Apartment Type' />
+            <button>Search</button>
+          </div>
+        </div>
+        <p id='discover'><a href="#"><BsArrowDownCircleFill /></a> Discover more offers and properties</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './navbar.css'
+import { RiHomeHeartFill } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar container'>
+      <div className="navbar__main">
+        <div className="logo">
+          <RiHomeHeartFill className='logo__icon'/>
+          <h3>DOMYS</h3>
+        </div>
+        <div className='divide__line'></div>
+        <div className="navbar__links">
+          <a href="#property">Properties</a>
+          <a href="#listing">Listings</a>
+          <a href="#about">About</a>
+        </div>
+      </div>
+      <div className="navbar__sign">
+        <a href="#get-started">Sign Up</a>
+        <a href="#">Sign In</a>
+      </div>
+    </div>
   )
 }
 
