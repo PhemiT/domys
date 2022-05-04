66eb5cef6dcc76f048925f44d26dd4a6a1fc8344
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-05-04 18:47:41 +0100

    img fix

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -34,7 +34,7 @@ const Featured = () => {
       <h1>Featured Properties</h1>
       <div className="featured__listings">
         <Listing price='$650,000' title='Plaza by the Sea' address='2051 Echo Lane, Grand Rapids' image={property1} imageWebp={property1webp} owner='Ernest Taylor' ownerImage={owner1} ownerImageWebp={owner1webp} />
-        <Listing price='$900,000' title='Quaint Modern Home' address='1755 Glendale Avenue, Woodland Hills' image={property2} imageWebp={property2webp} owner='Richard Doe' ownerImage={owner2} ownerImageWebp={owner2webp} />
+        <Listing price='$900,000' title='Modern Home' address='1755 Glendale Avenue, Woodland Hills' image={property2} imageWebp={property2webp} owner='Richard Doe' ownerImage={owner2} ownerImageWebp={owner2webp} />
         <Listing price='$3,000,000' title='Smart Luxury Duplex' address='3975 Yorkshire Circle, Greenville' image={property3} imageWebp={property3webp} owner='Walter Lay' ownerImage={owner3} ownerImageWebp={owner3webp} />
         <Listing price='$3,450,000' title='Glass Dome' address='1054 Randall Drive, Honolulu' image={property4} imageWebp={property4webp} owner='Sophia Gonzales' ownerImage={owner4} ownerImageWebp={owner4webp} />
         <Listing price='$2,000,000' title='Boulevard Mansion' address='1600 Eagle Street, Belleville' image={property5} imageWebp={property5webp} owner='Karen Gilmore' ownerImage={owner5} ownerImageWebp={owner5webp} />
