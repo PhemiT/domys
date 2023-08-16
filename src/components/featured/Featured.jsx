a878d19b887f6eac98c7b132f031174c2a42c109
Olufemi Olubukunmi <phemiwrites@gmail.com>
2023-08-16 10:14:51 +0100

    more listings

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,6 +5,9 @@ import {
   property1,
   property2,
   property3,
+  property4,
+  property5,
+  property6,
   realtor1,
   realtor2,
   realtor3
@@ -16,8 +19,11 @@ const Featured = () => {
       <h1>Featured Properties</h1>
       <div className="featured__listings">
         <Listing price='$650,000' title='Plaza by the Sea' address='2051 Echo Lane, Grand Rapids' image={property1} owner='Ernest Taylor' ownerImage={realtor1} />
-        <Listing price='$900,000' title='Modern Home' address='1755 Glendale Avenue, Woodland Hills' image={property2} owner='Joan Summers' ownerImage={realtor2} />
+        <Listing price='$900,000' title='Modern Manor' address='1755 Glendale Avenue, Woodland Hills' image={property2} owner='Joan Summers' ownerImage={realtor2} />
         <Listing price='$3,000,000' title='Smart Luxury Duplex' address='3975 Yorkshire Circle, Greenville' image={property3} owner='Abbie Ablo' ownerImage={realtor3} />
+        <Listing price='$1,200,000' title='Poolside Mansion' address='1274 Lake Drive, Sunshine Valley' image={property4} owner='Ruth Smith' ownerImage={realtor2} />
+        <Listing price='$2,500,000' title='Not Haunted Villa' address='6597 Pine Hill Road, Misty Peaks' image={property5} owner='Mia Ablo' ownerImage={realtor3} />
+        <Listing price='$800,000' title='Urban Loft' address='992 Downtown Street, Metro City' image={property6} owner='Arnett Taylor' ownerImage={realtor1} />
       </div>
       <div className="ff__cta">
         <a href="#discover" className='featured__cta'>Discover More</a>
--- src/components/featured/imports.js
+++ src/components/featured/imports.js
@@ -1,6 +1,9 @@
 import property1 from '../../assets/property1.jpg';
 import property2 from '../../assets/property2.jpg';
 import property3 from '../../assets/property3.jpg';
+import property4 from '../../assets/property4.jpg';
+import property5 from '../../assets/property5.jpg';
+import property6 from '../../assets/property6.jpg';
 import realtor1 from '../../assets/realtor1.webp';
 import realtor2 from '../../assets/realtor2.webp';
 import realtor3 from '../../assets/realtor3.webp';
@@ -10,6 +13,9 @@ export {
     property1,
     property2,
     property3,
+    property4,
+    property5,
+    property6,
     realtor1,
     realtor2,
     realtor3
